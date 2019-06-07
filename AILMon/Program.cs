b408d70a6f8a6162838c4bd5510c307d5b564141
Arawn Davies <arawn.davies@gmail.com>
2019-06-07 23:07:31 +0100

    reference Apollo IL

---
+++ AILMon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AILMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
